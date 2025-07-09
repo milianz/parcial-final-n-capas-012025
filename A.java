43287913d13e19612f267e4ee807948f43d1e8e5
milianz <00053722@uca.edu.sv>
2025-07-08 19:06:50 -0600

    Add JWT dependencies for token handling

--- A.java
+++
@@ -1,4 +0,0 @@
-touch src/main/java/com/uca/parcialfinalncapas/dto/request/LoginRequest.java
-touch src/main/java/com/uca/parcialfinalncapas/dto/request/RegisterRequest.java
-touch src/main/java/com/uca/parcialfinalncapas/dto/response/AuthResponse.java
-touch src/main/java/com/uca/parcialfinalncapas/dto/response/JwtResponse.java
